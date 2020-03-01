6dc35e10a706b08971a0dfa054d30a62e868879b
danielsind <danielsindet@gmail.com>
2020-03-01 23:33:59 +0300

    Added some changes to Quotes.java

--- app/src/main/java/com/moringa/inspire/QuotesActivity.java
+++ app/src/main/java/com/moringa/inspire/QuotesActivity.java
@@ -1,11 +1,13 @@
 package com.moringa.inspire;
 
+import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class QuotesActivity extends AppCompatActivity {
     @BindView(R.id.quoteTextView) TextView mLocationTextView;
@@ -14,5 +16,11 @@ public class QuotesActivity extends AppCompatActivity {
     private String[] quotes = new String[] {"Work Hard","Be Positive"};
     private String[] author = new String[]{"James","Danny"};
 
+@Override
+    protected void onCreate(Bundle savedInstanceState){
+    super .onCreate(savedInstanceState);
+    setContentView(R.layout.activity_quotes);
+    ButterKnife.bind(this);
 
+}
 }
