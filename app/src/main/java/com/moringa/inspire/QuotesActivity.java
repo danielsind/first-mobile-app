c1a2273ebf8616f9bdc31277b75b1fab754a2b70
danielsind <danielsindet@gmail.com>
2020-03-01 23:24:38 +0300

    Declared a string at QuotesActivity.java class, String Array quote and Author

--- app/src/main/java/com/moringa/inspire/QuotesActivity.java
+++ app/src/main/java/com/moringa/inspire/QuotesActivity.java
@@ -8,6 +8,9 @@ import butterknife.BindView;
 public class QuotesActivity {
     @BindView(R.id.quoteTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
-    public static final String TAG =
+    public static final String TAG = QuotesActivity.class.getSimpleName();
+    private String[] quotes = new String[] {"Work Hard","Be Positive"};
+    private String[] author = new String[]{"James","Danny"};
+
 
 }
