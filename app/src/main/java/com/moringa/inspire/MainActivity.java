56b7c28e44724ebbefd17f30d180060b2ed4a718
danielsind <danielsindet@gmail.com>
2020-03-02 00:29:27 +0300

    implelment the onclickMethod to allow switch between the layouts

--- app/src/main/java/com/moringa/inspire/MainActivity.java
+++ app/src/main/java/com/moringa/inspire/MainActivity.java
@@ -2,12 +2,33 @@ package com.moringa.inspire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findQuotesButton) Button mFindQuotesButton;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindQuotesButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v == mFindQuotesButton){
+            Intent intent = new Intent(MainActivity.this, QuotesActivity.class);
+            startActivity(intent);
+        }
     }
 }
