4f0bcc6e372e52c6ac33efa1e79dc8671f0b3235
danielsind <danielsindet@gmail.com>
2020-03-02 00:04:10 +0300

    Renamed the ID for Button And the Other buttons were deleted

--- app/src/main/java/com/moringa/inspire/QuotesActivity.java
+++ app/src/main/java/com/moringa/inspire/QuotesActivity.java
@@ -1,8 +1,12 @@
 package com.moringa.inspire;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +14,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class QuotesActivity extends AppCompatActivity {
-    @BindView(R.id.quoteTextView) TextView mLocationTextView;
+    @BindView(R.id.quoteTextView) TextView mQuoteTextView;
     @BindView(R.id.listView) ListView mListView;
     public static final String TAG = QuotesActivity.class.getSimpleName();
     private String[] quotes = new String[] {"Work Hard","Be Positive"};
@@ -21,6 +25,15 @@ public class QuotesActivity extends AppCompatActivity {
     super .onCreate(savedInstanceState);
     setContentView(R.layout.activity_quotes);
     ButterKnife.bind(this);
+    MyQuotesAdapter adapter = new MyQuotesAdapter(this,android.R.layout.simple_list_item_1,quotes,author);
+    mListView.setAdapter(adapter);
+    mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String quote = ((TextView)view).getText().toString();
+            Toast.makeText(QuotesActivity.this,quote, Toast.LENGTH_SHORT).show();
+        }
+    });
 
 }
 }
