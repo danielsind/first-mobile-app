ce3c96060b53a54d1db6a06ebdabecd2a8c64cc9
danielsind <danielsindet@gmail.com>
2020-03-01 23:28:37 +0300

    Added an extend class to QuoteActivity -- to extend AppCompatActivity

--- app/src/main/java/com/moringa/inspire/QuotesActivity.java
+++ app/src/main/java/com/moringa/inspire/QuotesActivity.java
@@ -3,9 +3,11 @@ package com.moringa.inspire;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import butterknife.BindView;
 
-public class QuotesActivity {
+public class QuotesActivity extends AppCompatActivity {
     @BindView(R.id.quoteTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
     public static final String TAG = QuotesActivity.class.getSimpleName();
