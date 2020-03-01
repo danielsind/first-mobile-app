7283e8f47555b89f683d575575e8a36652e1d7db
danielsind <danielsindet@gmail.com>
2020-03-01 23:44:09 +0300

    Added an ArrayAdapter MyQuotesAdapter that will be binding the string to the view

--- app/src/main/java/com/moringa/inspire/MyQuotesAdapter.java
+++ app/src/main/java/com/moringa/inspire/MyQuotesAdapter.java
@@ -1,4 +1,23 @@
 package com.moringa.inspire;
 
-public class MyQuotesAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyQuotesAdapter  extends ArrayAdapter {
+    private String[] mQuotes;
+    private String[] mAuthor;
+    private Context mContext;
+
+    public MyQuotesAdapter(Context mContext, int resource , String[] mAuthor , String[] mQuotes){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mAuthor = mAuthor;
+        this.mQuotes = mQuotes;
+    }
+    @Override
+    public Object getItem(int position){
+        String author = mAuthor[position];
+        String quote = mQuotes[position];
+        return String.format("%s \nAuthor :  %s", quote, author);
+    }
 }
